3bf3b9ea411c66864e0707ce69103add544d9976
sammykhaleel <sammykhaleel83@gmail.com>
2021-03-21 02:36:21 +0200

    pushing

--- components/Chat.js
+++ components/Chat.js
@@ -1,75 +1,3 @@
-// import React from "react";
-// import { GiftedChat, Bubble } from "react-native-gifted-chat";
-
-// import {
-//   View,
-//   Text,
-//   Button,
-//   Platform,
-//   KeyboardAvoidingView,
-// } from "react-native";
-
-// export default class Chat extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       messages: [],
-//     };
-//   }
-//   componentDidMount() {
-//     this.setState({
-//       messages: [
-//         {
-//           _id: 1,
-//           text: "Hello developer",
-//           createdAt: new Date(),
-//           user: {
-//             _id: 2,
-//             name: "React Native",
-//             avatar: "https://placeimg.com/140/140/any",
-//           },
-//         },
-//       ],
-//     });
-//   }
-
-//   onSend(messages = []) {
-//     this.setState((previousState) => ({
-//       messages: GiftedChat.append(previousState.messages, messages),
-//     }));
-//   }
-//   renderBubble(props) {
-//     return (
-//       <Bubble
-//         {...props}
-//         wrapperStyle={{
-//           right: {
-//             backgroundColor: "#000",
-//           },
-//         }}
-//       />
-//     );
-//   }
-//   render() {
-//     let name = this.props.route.params.name;
-//     this.props.navigation.setOptions({ title: name });
-//     return (
-//       <View style={{ flex: 1 }}>
-//         <GiftedChat
-//           renderBubble={this.renderBubble.bind(this)}
-//           messages={this.state.messages}
-//           onSend={(messages) => this.onSend(messages)}
-//           user={{
-//             _id: 1,
-//           }}
-//         />
-//         {Platform.OS === "android" ? (
-//           <KeyboardAvoidingView behavior="height" />
-//         ) : null}
-//       </View>
-//     );
-//   }
-// }
 import React from "react";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
@@ -106,28 +34,7 @@ export default class Chat extends React.Component {
       messages: [],
     };
   }
-  // componentDidMount() {
-  //   this.setState({
-  // messages: [
-  //   {
-  //     _id: 1,
-  //     text: "Hello developer",
-  //     createdAt: new Date(),
-  //     user: {
-  //       _id: 2,
-  //       name: "React Native",
-  //       avatar: "https://placeimg.com/140/140/any",
-  //     },
-  //   },
-  // ],
-  //   });
-  // }
-  // componentDidMount() {
-  //   this.referenceChatMessages = firebase.firestore().collection("messages");
-  //   this.unsubscribe = this.referenceChatMessages.onSnapshot(
-  //     this.onCollectionUpdate
-  //   );
-  // }
+
   componentDidMount() {
     this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
       if (!user) {
@@ -167,11 +74,6 @@ export default class Chat extends React.Component {
     });
   };
 
-  // onSend(messages = []) {
-  //   this.setState((previousState) => ({
-  //     messages: GiftedChat.append(previousState.messages, messages),
-  //   }));
-  // }
   addMessage() {
     const message = this.state.messages[0];
     this.referenceChatMessages.add({
